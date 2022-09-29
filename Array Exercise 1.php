f23fd7f8ec0a396710fc3ad935b97460c6cbabc6
Your Name <you@example.com>
2022-09-28 20:36:43 -0800

    Upload successfully

---
+++ Array Exercise 1.php
@@ -0,0 +1,10 @@
+
+<h2>Array Exercise 1</h2>
+
+<?php
+
+$weatherConditions = array("rain","sunshine","clouds","hail","sleet","snow","wind");
+
+echo "We've seen all kinds of weather this month. At the beginning of the month, we had " . $weatherConditions[5] . " and " . $weatherConditions[6] . ". Then came " . $weatherConditions[1] . " with a few " . $weatherConditions[2] . " and some  " . $weatherConditions[0] . ". At least we didn't get any " . $weatherConditions[3] . " or " . $weatherConditions[4] . ". ";
+
+?>
